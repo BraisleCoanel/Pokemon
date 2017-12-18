d22294aaf11e7cd998f89dc2b2f621a124b0ff57
BraisleCoanel <31518773+BraisleCoanel@users.noreply.github.com>
2017-12-18 11:52:31 -0700

    Implemented Dark into Greninja and added code into void constructors.

--- src/pokemon/model/Greninja.java
+++ src/pokemon/model/Greninja.java
@@ -1,6 +1,6 @@
 package pokemon.model;
 
-public class Greninja extends Pokemon implements Water
+public class Greninja extends Pokemon implements Water, Dark
 {
 	public Greninja(int number, String name)
 	{
@@ -9,18 +9,33 @@ public class Greninja extends Pokemon implements Water
 	
 	public int waterShuriken(int amount)
 	{
-		int sample = 0;
+		int shuriken = 5;
 		
-		return sample;
+		return shuriken;
 	}
 	
 	public String waterPulse()
 	{
-		return "The oppenent is now confused";
+		return "The oppenent is now confused.";
 	}
 	
 	public void hydroPump()
 	{
-		
+		System.out.println("The target is blasted by a huge volume of water launched under great pressure.");
+	}
+	
+	public void darkPulse()
+	{
+		System.out.println("The user releases a horrible aura imbued with dark thoughts.");
+	}
+	
+	public void feintAttack()
+	{
+		System.out.println("The user approaches the target disarmingly, then throws a sucker punch.");
+	}
+	
+	public String taunt()
+	{
+		return "The opponent is now enraged.";
 	}
 }
