c073dc7c3847fcbf4bfa36c25a37d4f5e26e75ae
BraisleCoanel <31518773+BraisleCoanel@users.noreply.github.com>
2018-01-08 11:57:18 -0700

    Added setups for Greninja

--- src/pokemon/model/Greninja.java
+++ src/pokemon/model/Greninja.java
@@ -2,11 +2,30 @@ package pokemon.model;
 
 public class Greninja extends Pokemon implements Water, Dark
 {
+	public Greninja()
+	{
+		super(658, "Greninja");
+		setup();
+	}
+	
+	public Greninja(String name)
+	{
+		super(658, name);
+		setup();
+	}
 	public Greninja(int number, String name)
 	{
 		super(number, name);
+		setup();
 	}
 	
+	protected void setup()
+	{
+		this.setAttackPoints(95);
+		this.setCanEvolve(false);
+		this.setEnhancmentModifier(.05);
+		this.setHealthPoints(72);
+	}
 	public int waterShuriken(int amount)
 	{
 		int shuriken = (int)(Math.random()*4)+2;
