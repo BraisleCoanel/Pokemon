c8140327818d546cb0cb1cee3ddf0eb8dc79a20e
BraisleCoanel <31518773+BraisleCoanel@users.noreply.github.com>
2018-01-08 13:45:12 -0700

    Added imports and all of the "private JStuff otherStuff;"

--- src/pokemon/controller/PokedexPanel.java
+++ src/pokemon/controller/PokedexPanel.java
@@ -1,8 +1,54 @@
 package pokemon.controller;
 
-public class PokedexPanel
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+import javax.swing.*;
+import pokemon.controller.PokemonController;
+
+public class PokedexPanel extends JPanel
 {
-	super();
+	private PokemonController appController;
+	private SpringLayout appLayout;
 	
-	iconLabel = newJLabel("", new ImageIcon(getClass().getResource("/pokemon/view/images/Pokeball.png")), JLabel.CENTER);
+	private JLabel healthLabel;
+	private JLabel attackLabel;
+	private JLabel nameLabel;
+	private JLabel numberLabel;
+	private JLabel evolvableLabel;
+	private JLabel modifierLabel;
+	private JLabel iconLabel;
+	
+	private JCheckBox evolvableBox;
+	private JTextField nameField;
+	private JTextField numberFlield;
+	private JTextField attackField;
+	private JTextField healthField;
+	private JTextField modifierField;
+	
+	private JTextArea descriptionArea;
+	private JTextArea typeArea;
+	
+	private JButton saveButton;
+	private JButton clearButton;
+	private JComboBox pokedexDropdown;
+	
+	private JPanel firstType; 
+	private JPanel secondType
+	
+	
+	public PokedexPanel(PokemonController appController)
+	{
+		super();
+		this.appController = appController;
+		
+		appLayout = new SpringLayout();
+		
+		evolvableBox = JCheckBox();
+	
+		iconLabel = newJLabel("", new ImageIcon(getClass().getResource("/pokemon/view/images/Pokeball.png")), JLabel.CENTER);
+	}
+	
 }
--- src/pokemon/view/PokemonFrame.java
+++ src/pokemon/view/PokemonFrame.java
@@ -1,6 +1,21 @@
 package pokemon.view;
 
-public class PokemonFrame
-{
+import pokemon.controller.PokedexPanel;
+import javax.swing.JFrame;
+import pokemon.controller.PokemonController;
 
+public class PokemonFrame extends JFrame
+{
+	private PokedexPanel pokePanel;
+	private PokemonController appController;
+	
+	public PokemonFrame(PokemonController appController)
+	{
+//		super();
+//		this.appController = appController;
+//		this.pokePanel = new PokedexPanel(appController);
+//		
+//		setupFrame();
+	}
+	
 }
